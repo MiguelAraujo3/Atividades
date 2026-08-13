705cb5ed4f031e740619560ddf5c2c5a2f8da8b3
MiguelAraujo3 <miguelaraujo16052008@gmail.com>
2026-08-13 15:10:55 -0300

    feat: exercícios da aula 04 de AED

--- Algoritmos-e-Estrutura-de-Dados/aula03/ex02.py
+++ Algoritmos-e-Estrutura-de-Dados/aula03/ex02.py
@@ -5,8 +5,18 @@ class Conta:
         self.__saldo = 0
 
     def depositar(self, valor):
-        self.__saldo += valor
-
+        if valor > 0:
+            self.__saldo += valor
+        else: 
+            return "Valor inválido para depósito"
+    
+    def sacar(self, valor): 
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+        elif valor < 0:
+            return "Valor Inválido"
+        else:
+            return "Solicite o saldo para saber quanto pode sacar"
     @property
     def numero(self):
         return self.__numero
@@ -31,12 +41,14 @@ class Conta:
     def saldo(self, saldo):
         self.__saldo = saldo
 
-conta = Conta(12312,'Miguel')
-print(f'Número: {conta.numero} Titular: {conta.titular} Saldo: {conta.saldo}')
+    def imprimir(self):
+        print(f'Número: {self.__numero} Titular: {self.__titular} Saldo: {self.__saldo}')  
 
+conta = Conta(12312,'Miguel')
+conta.imprimir()
 conta.numero = 31231
 conta.titular = 'Allan'
-print(f'Número: {conta.numero} Titular: {conta.titular} Saldo: {conta.saldo}')
+conta.imprimir()
 conta.depositar(float(input("Digite o valor do deposito: ")))
-print(f'Número: {conta.numero} Titular: {conta.titular} Saldo: {conta.saldo}')
-
+conta.sacar(float(input("Digite o valor do saque: ")))
+conta.imprimir()
