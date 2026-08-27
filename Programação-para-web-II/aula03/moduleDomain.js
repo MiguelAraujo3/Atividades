fdb2d0e7ce2da25060441aedf935ea66fa951ee1
Miguel Araújo Linux <miguelaraujo16052008@gmail.com>
2026-08-26 22:41:52 -0300

    feat: exercicios de modulo pw2

---
+++ Programação-para-web-II/aula03/moduleDomain.js
@@ -0,0 +1,12 @@
+function soma(param1, param2) {
+  return param1 + param2
+}
+export function subtrai(param1, param2) {
+  return param1 - param2
+}
+function multiplica(param1, param2) {
+  return param1 * param2
+}
+
+export {soma} ;
+export default multiplica;
